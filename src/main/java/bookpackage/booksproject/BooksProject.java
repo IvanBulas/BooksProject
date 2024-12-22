d2be520cf02bb9ac0c868c72a5233244e7ec7d43
IvanBulas <84940328+IvanBulas@users.noreply.github.com>
2024-12-22 18:41:43 +0100

    //1st
    //Java BooksProject

---
+++ src/main/java/bookpackage/booksproject/BooksProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bookpackage.booksproject;
+
+/**
+ *
+ * @author ibulas
+ */
+public class BooksProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
